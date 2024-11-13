76275a0db25dee99339961213021875ca4ae6004
gwanyoungkim719 <0000kgy@naver.com>
2024-11-14 02:32:29 +0900

    [Add] Coin.cs

--- Assets/Scripts/Object/Data/Coin.cs
+++ Assets/Scripts/Object/Data/Coin.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            bool getId = TryGetComponent<Placement>(out Placement placement);
+            PlacementManager.Instance.ReturnPlacement(placement.placementSO.PlacementId, this.gameObject);
+        }
     }
 }
