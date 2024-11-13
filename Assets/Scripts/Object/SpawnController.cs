e1896a0dd5ab3c98e5adfddd786743460af695d8
gwanyoungkim719 <0000kgy@naver.com>
2024-11-13 11:42:43 +0900

    [Update] Direction Decide Logic

---
+++ Assets/Scripts/Object/SpawnController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    private EDirection eDirection;
+
+    public SpawnPoint leftPoint;
+    public SpawnPoint rightPoint;
+
+    private void Start()
+    {
+        leftPoint.enabled = false;
+        rightPoint.enabled = false;
+        DecideDirection();
+    }
+
+    private void DecideDirection()
+    {
+        eDirection =  (EDirection)Random.Range(0, 2); //무작위로 방향을 설정
+
+        switch(eDirection)
+        {
+            case EDirection.left:
+                leftPoint.enabled = true; break;
+            case EDirection.right:
+                rightPoint.enabled = true; break;
+        }
+    }
+}
